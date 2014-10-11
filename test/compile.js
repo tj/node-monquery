93de5ce07cfa71e02584955ef20ee3de410106ed
TJ Holowaychuk <tj@vision-media.ca>
2014-10-11 11:58:27 -0700

    add operator whitespace support and clean more stuff up

--- test/compile.js
+++ test/compile.js
@@ -19,6 +19,14 @@ describe('operators', function(){
         { level: 'alert' }
       ]
     });
+
+    var ret = query('level: error OR level: alert');
+    ret.should.eql({
+      $or: [
+        { level: 'error' },
+        { level: 'alert' }
+      ]
+    });
   })
 
   it('should compile when nested', function(){
@@ -32,44 +40,69 @@ describe('operators', function(){
   })
 })
 
-describe('comparison', function () {
-  it('should compile greater than', function () {
+describe('comparison', function(){
+  it('should compile greater than', function(){
     var ret = query('level>5');
+    ret.should.eql({
+      level: { $gt: 5 }
+    });
+
+    var ret = query('level > 5');
     ret.should.eql({
       level: { $gt: 5 }
     });
   })
 
-  it('should compile greater equal than', function () {
+  it('should compile greater equal than', function(){
     var ret = query('level>=5');
+    ret.should.eql({
+      level: { $gte: 5 }
+    });
+
+    var ret = query('level >= 5');
     ret.should.eql({
       level: { $gte: 5 }
     });
   })
 
-  it('should compile less than', function () {
+  it('should compile less than', function(){
     var ret = query('level<5');
+    ret.should.eql({
+      level: { $lt: 5 }
+    });
+
+    var ret = query('level < 5');
     ret.should.eql({
       level: { $lt: 5 }
     });
   })
 
-  it('should compile less equal than', function () {
+  it('should compile less equal than', function(){
     var ret = query('level<=5');
+    ret.should.eql({
+      level: { $lte: 5 }
+    });
+
+    var ret = query('level <= 5');
     ret.should.eql({
       level: { $lte: 5 }
     });
   })
 
-  it('should compile not equal', function () {
+  it('should compile not equal', function(){
     var ret = query('level!=5');
+    ret.should.eql({
+      level: { $ne: 5 }
+    });
+
+    var ret = query('level != 5');
     ret.should.eql({
       level: { $ne: 5 }
     });
   })
 
-  it('should compile nested', function () {
-    var ret = query('(age>20 AND age<50) OR gender:male');
+  it('should compile nested', function(){
+    var ret = query('(age > 20 AND age < 50) OR gender:male');
     ret.should.eql({
       $or: [
         { $and: [ { age: { $gt: 20 } }, { age: { $lt: 50 } } ] },
